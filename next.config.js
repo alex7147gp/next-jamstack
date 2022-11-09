2ccd0b0533c15e4721fe13c10f814cfdc7fcd0c7
alex7147gp <71793852+alex7147gp@users.noreply.github.com>
2022-11-09 16:33:55 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const config = {
   future: {
     webpack5: true,
   },
-  env: {
-    NEXT_PUBLIC_SPACE_ID: 'ehut0i2fzqdk',
-    NEXT_PUBLIC_ACCESS_TOKEN: 'VG4rSRnnYtud1TNQ_UQW1HIT00GJYfVAmJ0Lh8A3CTc',
-  },
 }
 
 module.exports = withBundleAnalyzer(config)
