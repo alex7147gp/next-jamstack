36c323b474caf7ed8e23e641b2cfb86138829aec
alex7147gp <johncedillo77@gmail.com>
2022-11-14 16:46:19 -0500

    Fix bad repo

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const config = {
+  future: {
+    webpack5: true,
+  },
+
+  images: {
+    domains: ['images.ctfassets.net'],
+  }
+}
+
+module.exports = withBundleAnalyzer(config)
